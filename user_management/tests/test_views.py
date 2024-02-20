69403bab99fff85e5fb761f9039e4949b7635cff
ChrisLoucaides <christosloucaides@gmail.com>
2024-02-20 18:28:10 +0000

    refactored login tests

--- user_management/tests/test_views.py
+++ user_management/tests/test_views.py
@@ -5,32 +5,70 @@ from django.urls import reverse
 from user_management.models import Account
 from user_management.views import user_login, signup
 
+AN_INVALID_CREDENTIALS_MESSAGE = 'Invalid username or password'
 
-class UserLoginTestCase(TestCase):  # TODO: refactor me
+A_VALID_USERNAME = 'testuser'
+A_VALID_PASSWORD = 'testpassword'
+AN_INVALID_PASSWORD = 'invalid'
+
+
+class UserLoginTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.username = 'testuser'
-        self.password = 'testpassword'
+        self.username = A_VALID_USERNAME
+        self.password = A_VALID_PASSWORD
         self.user = Account.objects.create_user(username=self.username, password=self.password)
 
     def test_login_successful(self):
-        request = self.factory.post(reverse('login'), {'username': self.username, 'password': self.password})
-        middleware = SessionMiddleware(lambda x: None)
-        middleware.process_request(request)
-        request.session.save()
+        request = self.given_a_login_post_request_with_credentials(self.username, self.password)
 
-        response = user_login(request)
-        self.assertEqual(response.status_code, 302)
+        response = self.when_we_attempt_authentication(request)
+
+        self.then_the_user_is_logged_in_and_redirected(response)
 
     def test_invalid_credentials(self):
-        request = self.factory.post(reverse('login'), {'username': self.username, 'password': 'wrongpassword'})
-        response = user_login(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Invalid username or password')
+        request = self.given_a_login_post_request_with_credentials(self.username, AN_INVALID_PASSWORD)
+
+        response = self.when_we_attempt_authentication(request)
+
+        self.the_user_is_not_logged_in_and_the_login_page_is_re_displayed(response)
+        self.and_the_response_contains(response, AN_INVALID_CREDENTIALS_MESSAGE)
 
     def test_get_request(self):
+        request = self.given_a_login_get_request()
+
+        response = self.when_we_attempt_authentication(request)
+
+        self.the_user_is_not_logged_in_and_the_login_page_is_re_displayed(response)
+
+    def given_a_login_get_request(self):
         request = self.factory.get(reverse('login'))
+        return request
+
+    def given_a_login_post_request_with_credentials(self, username, password):
+        request = self.factory.post(reverse('login'), {'username': username, 'password': password})
+
+        def get_response():
+            return None
+
+        # noinspection PyTypeChecker
+        middleware = SessionMiddleware(get_response)  # Create session as it is needed to create middleware object
+        middleware.process_request(request)
+        return request
+
+    def then_the_user_is_logged_in_and_redirected(self, response):
+        self.assertEqual(response.status_code, 302)
+
+    @staticmethod
+    def when_we_attempt_authentication(request):
         response = user_login(request)
+        return response
+
+    def and_the_response_contains(self, response, response_message):
+        self.assertContains(response, response_message)
+
+    def the_user_is_not_logged_in_and_the_login_page_is_re_displayed(self,
+                                                                     response):  # TODO: Revisit this, add assertion to check for login page?
         self.assertEqual(response.status_code, 200)
 
 
@@ -40,9 +78,9 @@ class SignUpTestCase(TestCase):  # TODO: refactor me
 
     def test_signup_success(self):
         request = self.factory.post(reverse('signup'), {
-            'username': 'testuser',
-            'password': 'testpassword',
-            'confirm_password': 'testpassword',
+            'username': A_VALID_USERNAME,
+            'password': A_VALID_PASSWORD,
+            'confirm_password': A_VALID_PASSWORD,
             'email': 'test@example.com',
             'first_name': 'Test',
             'last_name': 'User'
