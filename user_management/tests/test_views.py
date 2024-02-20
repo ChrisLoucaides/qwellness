4cb64cc37ddc00312d9b8e9f0fc62d66c572353c
ChrisLoucaides <christosloucaides@gmail.com>
2024-02-20 18:42:54 +0000

    refactored signup tests

--- user_management/tests/test_views.py
+++ user_management/tests/test_views.py
@@ -5,6 +5,12 @@ from django.urls import reverse
 from user_management.models import Account
 from user_management.views import user_login, signup
 
+AN_EXAMPLE_LAST_NAME = 'User'
+
+AN_EXAMPLE_FIRST_NAME = 'Test'
+
+A_VALID_EMAIL_ADDRESS = 'test@example.com'
+
 AN_INVALID_CREDENTIALS_MESSAGE = 'Invalid username or password'
 
 A_VALID_USERNAME = 'testuser'
@@ -72,29 +78,60 @@ class UserLoginTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class SignUpTestCase(TestCase):  # TODO: refactor me
+class SignUpTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
     def test_signup_success(self):
-        request = self.factory.post(reverse('signup'), {
-            'username': A_VALID_USERNAME,
-            'password': A_VALID_PASSWORD,
-            'confirm_password': A_VALID_PASSWORD,
-            'email': 'test@example.com',
-            'first_name': 'Test',
-            'last_name': 'User'
-        })
-        response = signup(request)
-        print(response.content)
-        self.assertEqual(response.status_code, 302)
+        request = self.given_a_sign_up_form_post_request_with(A_VALID_USERNAME, A_VALID_PASSWORD, A_VALID_PASSWORD,
+                                                              A_VALID_EMAIL_ADDRESS, AN_EXAMPLE_FIRST_NAME,
+                                                              AN_EXAMPLE_LAST_NAME)
+
+        response = self.when_we_try_and_register_a_new_user(request)
+
+        self.then_a_new_user_is_created_and_we_redirect_them(response)
 
     def test_signup_invalid_form(self):
-        request = self.factory.post(reverse('signup'), {})
-        response = signup(request)
-        self.assertEqual(response.status_code, 200)
+        request = self.given_an_invalid_signup_form_post_request()
+
+        response = self.when_we_try_and_register_a_new_user(request)
+
+        self.then_the_user_is_not_created_and_the_signup_page_is_redisplayed(response)
 
     def test_signup_get_request(self):
+        request = self.given_a_sign_up_form_get_request()
+
+        response = self.when_we_try_and_register_a_new_user(request)
+
+        self.then_the_user_is_not_created_and_the_signup_page_is_redisplayed(response)
+
+    def given_a_sign_up_form_get_request(self):
         request = self.factory.get(reverse('signup'))
-        response = signup(request)
+        return request
+
+    def given_a_sign_up_form_post_request_with(self, username, password, password_confirmation, email, first_name,
+                                               last_name):
+        request = self.factory.post(reverse('signup'), {
+            'username': username,
+            'password': password,
+            'confirm_password': password_confirmation,
+            'email': email,
+            'first_name': first_name,
+            'last_name': last_name
+        })
+        return request
+
+    def then_a_new_user_is_created_and_we_redirect_them(self, response):
+        self.assertEqual(response.status_code, 302)
+
+    def then_the_user_is_not_created_and_the_signup_page_is_redisplayed(self, response):
         self.assertEqual(response.status_code, 200)
+
+    def given_an_invalid_signup_form_post_request(self):
+        request = self.factory.post(reverse('signup'), {})
+        return request
+
+    @staticmethod
+    def when_we_try_and_register_a_new_user(request):
+        response = signup(request)
+        return response
