0a597cb908451c19600318038eca304dc55ca724
ChrisLoucaides <christosloucaides@gmail.com>
2024-03-04 10:14:21 +0000

    FYP-10 Refactoring happy path test

--- user_management/tests/test_get_user_info.py
+++ user_management/tests/test_get_user_info.py
@@ -10,20 +10,20 @@ class TestGetUserInfo(TestCase):
         self.factory = RequestFactory()
         self.User = get_user_model()
 
-    def test_get_user_info_authenticated(self):
-        # Given
-        user = self.User.objects.create_user(username='test_user', password='test_password', first_name='Test',
-                                             role='admin')
-        user.advisor = self.User.objects.create_user(username='test_advisor', password='test_password',
-                                                     first_name='Advisor')
-        user.save()
-        request = self.factory.get('/get-user-info/')
-        request.user = user
+    def test_should_return_authenticated_user_data(self):
+        user = self.given_an_authenticated_student_user_with_credentials('test_user', 'test_password', 'Test')
+        user.advisor = self.and_an_advisor_with_credentials('test_advisor', 'test_password', 'Advisor')
+        self.and_the_user_is_saved(user)
 
-        # When
-        response = get_user_info(request)
+        response = self.when_a_request_is_made(self.to_the_get_user_info_endpoint_with_user(user))
 
-        # Then
+        expected_data = self.then_the_response_comes_back_as_200(response, user)
+        self.and_the_content_matches_with_our_expected_data(expected_data, response)
+
+    def and_the_content_matches_with_our_expected_data(self, expected_data, response):
+        self.assertEqual(json.loads(response.content), expected_data)
+
+    def then_the_response_comes_back_as_200(self, response, user):
         self.assertEqual(response.status_code, 200)
         expected_data = {
             "id": user.id,
@@ -32,7 +32,28 @@ class TestGetUserInfo(TestCase):
             "role": user.role,
             "advisor": user.advisor.username if user.advisor else None
         }
-        self.assertEqual(json.loads(response.content), expected_data)
+        return expected_data
+
+    def to_the_get_user_info_endpoint_with_user(self, user):
+        request = self.factory.get('/get-user-info/')
+        request.user = user
+        return request
+
+    def and_an_advisor_with_credentials(self, username, password, first_name):
+        return self.User.objects.create_user(username=username, password=password,
+                                             first_name=first_name)
+
+    def given_an_authenticated_student_user_with_credentials(self, username, password, first_name):
+        return self.User.objects.create_user(username=username, password=password, first_name=first_name,
+                                             role='student')
+
+    @staticmethod
+    def when_a_request_is_made(request):
+        return get_user_info(request)
+
+    @staticmethod
+    def and_the_user_is_saved(user):
+        user.save()
 
     def test_get_user_info_unauthenticated(self):
         # Given
