d581d5fca44c7eb06cd7fada7faee4fd499fa56e
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-05 21:04:51 +0100

    FYP-22 refactoring test

--- task_management/tests/test_create_student_task.py
+++ task_management/tests/test_create_student_task.py
@@ -4,51 +4,89 @@ from task_management.models import Task
 from user_management.models import Student
 
 
-class CreateTaskTest(TestCase):  # TODO: FYP-22 Refactor me
+class CreateTaskTest(TestCase):
     def setUp(self):
         self.client = Client()
-        # Create a test student and log in
         self.student = Student.objects.create(username='test_student')
         self.client.force_login(self.student)
 
     def test_create_task_success(self):
-        # Given
+        data = self.given_a_valid_task_payload()
+
+        response = self.when_a_post_request_is_made_to_the_create_task_endpoint(data)
+
+        self.then_a_new_task_is_created(response)
+
+        response_data = json.loads(response.content)
+        self.and_we_get_a_success_response(response_data)
+
+        task_id = response_data['task_id']
+        self.and_the_task_has_been_added_to_the_database(task_id)
+
+        Task.objects.filter(pk=task_id).delete()  # Clean Up
+
+    def and_the_task_has_been_added_to_the_database(self, task_id):
+        self.assertIsNotNone(Task.objects.filter(pk=task_id).first())
+
+    def and_we_get_a_success_response(self, response_data):
+        self.assertTrue(response_data['success'])
+
+    def then_a_new_task_is_created(self, response):
+        self.assertEqual(response.status_code, 201)
+
+    def when_a_post_request_is_made_to_the_create_task_endpoint(self, data):
+        return self.client.post('/create_task/', data=data)
+
+    @staticmethod
+    def given_a_valid_task_payload():
         data = {
             'username': 'test_student',
             'name': 'Test Task',
             'due_date': '2024-04-02',
             'description': 'Test Description'
         }
-        # When
-        response = self.client.post('/create_task/', data=data)
-        # Then
-        self.assertEqual(response.status_code, 201)
-        response_data = json.loads(response.content)
-        self.assertTrue(response_data['success'])
-        task_id = response_data['task_id']
-        self.assertIsNotNone(Task.objects.filter(pk=task_id).first())
-        # Clean up
-        Task.objects.filter(pk=task_id).delete()
+        return data
 
     def test_create_task_student_not_found(self):
-        # Given
+        data = self.given_an_invalid_student_in_the_payload()
+
+        response = self.when_a_post_request_is_made_to_the_create_task_endpoint(data)
+
+        self.then_we_get_a_404(response)
+
+        response_data = json.loads(response.content)
+        self.and_the_response_contains_a_student_not_found_error_message(response_data)
+
+    def and_the_response_contains_a_student_not_found_error_message(self, response_data):
+        self.assertEqual(response_data['error'], 'Student not found')
+
+    def then_we_get_a_404(self, response):
+        self.assertEqual(response.status_code, 404)
+
+    @staticmethod
+    def given_an_invalid_student_in_the_payload():
         data = {
             'username': 'non_existing_student',
             'name': 'Test Task',
             'due_date': '2024-04-02',
             'description': 'Test Description'
         }
-        # When
-        response = self.client.post('/create_task/', data=data)
-        # Then
-        self.assertEqual(response.status_code, 404)
-        response_data = json.loads(response.content)
-        self.assertEqual(response_data['error'], 'Student not found')
+        return data
 
     def test_create_task_method_not_allowed(self):
-        # When
-        response = self.client.get('/create_task/')
-        # Then
-        self.assertEqual(response.status_code, 405)
+        response = self.when_a_get_request_is_made_to_the_create_task_endpoint()
+
+        self.then_we_get_a_405(response)
+
         response_data = json.loads(response.content)
+        self.and_the_response_contains_a_method_not_allowed_message(response_data)
+
+    def and_the_response_contains_a_method_not_allowed_message(self, response_data):
         self.assertEqual(response_data['error'], 'Method not allowed')
+
+    def then_we_get_a_405(self, response):
+        self.assertEqual(response.status_code, 405)
+
+    def when_a_get_request_is_made_to_the_create_task_endpoint(self):
+        response = self.client.get('/create_task/')
+        return response
