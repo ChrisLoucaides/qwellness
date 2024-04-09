c3f38e12bcbc15ba9317245d3dda55372c4706cc
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-09 14:28:03 +0100

    FYP-26 TODOs

--- task_management/urls.py
+++ task_management/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('create_task/', create_task, name='create_task'),
     path('get_student_tasks/', get_student_tasks, name='get_student_tasks'),
     path('update_task/', update_task, name='get_student_tasks')
-]
+]  # TODO FYP:26 Change endpoints to remove method names
