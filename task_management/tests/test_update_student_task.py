fd81ebd789d6a9d59542b26a4c24535f17773fbe
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-13 15:17:13 +0100

    FYP-24 minor refactoring

--- task_management/tests/test_update_student_task.py
+++ task_management/tests/test_update_student_task.py
@@ -26,7 +26,7 @@ class UpdateTaskTest(TestCase):
 
         response = self.when_the_user_makes_a_request_to_update_a_task(self.updated_task_data_with_no_id())
 
-        self.then_we_get_a_response_code(response, 400)
+        self.then_the_task_is_not_updated_and_we_get_a_response_code_of(response, 400)
         self.and_an_error_message_of(response, {'error': 'Task ID is required'})
 
     def test_should_not_update_task_given_invalid_id(self):
@@ -34,7 +34,7 @@ class UpdateTaskTest(TestCase):
 
         response = self.when_the_user_makes_a_request_to_update_a_task(self.updated_task_data_with_invalid_id())
 
-        self.then_we_get_a_response_code(response, 404)
+        self.then_the_task_is_not_updated_and_we_get_a_response_code_of(response, 404)
         self.and_an_error_message_of(response, {'error': 'Task not found'})
 
     def test_should_not_update_task_given_invalid_method(self):
@@ -42,13 +42,13 @@ class UpdateTaskTest(TestCase):
 
         response = self.when_a_user_makes_a_request_with_an_invalid_post_method()
 
-        self.then_we_get_a_response_code(response, 405)
+        self.then_the_task_is_not_updated_and_we_get_a_response_code_of(response, 405)
         self.and_an_error_message_of(response, {'error': 'Method not allowed'})
 
     def test_should_not_update_task_given_unauthenticated_user(self):
         response = self.when_the_user_makes_a_request_to_update_a_task(self.with_some_valid_task_data())
 
-        self.then_we_get_a_response_code(response, 302)
+        self.then_the_task_is_not_updated_and_we_get_a_response_code_of(response, 302)
 
     def given_the_student_is_logged_in(self):
         self.client.force_login(self.student)
@@ -70,7 +70,7 @@ class UpdateTaskTest(TestCase):
     def then_the_task_is_updated(self, response):
         self.assertEqual(response.status_code, 200)
 
-    def then_we_get_a_response_code(self, response, response_code):
+    def then_the_task_is_not_updated_and_we_get_a_response_code_of(self, response, response_code):
         self.assertEqual(response.status_code, response_code)
 
     def and_the_updated_task_has_description(self, updated_task, updated_task_description):
