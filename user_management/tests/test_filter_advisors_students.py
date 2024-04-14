fd8b9fd27d27d4a1999d7d151db47af2f8df0c34
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-14 10:39:46 +0100

    FYP-16 tidying up filter advisor students test

--- user_management/tests/test_filter_advisors_students.py
+++ user_management/tests/test_filter_advisors_students.py
@@ -26,6 +26,24 @@ class TestFilterAdvisorsStudents(TestCase):
         self.and_all_students_are_in_the_response(response_data)
         self.and_the_student_details_are_correct(response_data)
 
+    def test_should_not_returned_filtered_students_given_no_authentication(self):
+
+        request = self.when_a_non_authenticated_user_makes_a_request_to_get_a_list_of_their_students()
+
+        response = self.when_we_filter_the_advisors_students(request)
+
+        self.then_we_redirect_the_user_to_the_login_page(response)
+
+    def then_we_redirect_the_user_to_the_login_page(self, response):
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('Location', response.headers)
+        self.assertIn('/accounts/login/', response.headers['Location'])
+
+    def when_a_non_authenticated_user_makes_a_request_to_get_a_list_of_their_students(self):
+        request = self.factory.get('/filter-advisors-students/')
+        request.user = Mock(is_authenticated=False)
+        return request
+
     def and_the_student_details_are_correct(self, response_data):
         returned_student = response_data['students'][0]
         self.assertEqual(returned_student['username'], 'test_student')
@@ -56,24 +74,6 @@ class TestFilterAdvisorsStudents(TestCase):
     def given_an_advisor_with_username(username):
         advisor = Advisor.objects.create(username=username)
         return advisor
-
-    def test_should_not_returned_filtered_students_given_no_authentication(self):
-
-        request = self.when_a_non_authenticated_user_makes_a_request_to_get_a_list_of_their_students()
-
-        response = self.when_we_filter_the_advisors_students(request)
-
-        self.then_we_redirect_the_user_to_the_login_page(response)
-
-    def then_we_redirect_the_user_to_the_login_page(self, response):
-        self.assertEqual(response.status_code, 302)
-        self.assertIn('Location', response.headers)
-        self.assertIn('/accounts/login/', response.headers['Location'])
-
-    def when_a_non_authenticated_user_makes_a_request_to_get_a_list_of_their_students(self):
-        request = self.factory.get('/filter-advisors-students/')
-        request.user = Mock(is_authenticated=False)
-        return request
 
     @staticmethod
     def when_we_filter_the_advisors_students(request):
