dd3e97b58e609b92ba48742ce99ea35d76f3bf60
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-15 12:09:28 +0100

    FYP-28 created meeting model

--- meeting_management/models.py
+++ meeting_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user_management.models import Account
 
-# Create your models here.
+
+class Meeting(models.Model):
+    id = models.AutoField(primary_key=True)
+    student = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='student_meetings')
+    advisor = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='advisor_meetings')
+    date_and_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"Meeting between {self.student.username} and {self.advisor.username} at {self.date_and_time}"
