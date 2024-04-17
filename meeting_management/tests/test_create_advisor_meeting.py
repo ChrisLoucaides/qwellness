3727d0da0860899f7a9918cac1e6633b23766bb2
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-17 10:58:13 +0100

    FYP-33 extracting data into constants

--- meeting_management/tests/test_create_advisor_meeting.py
+++ meeting_management/tests/test_create_advisor_meeting.py
@@ -3,30 +3,41 @@ from django.urls import reverse
 from user_management.models import Advisor, Student
 import json
 
+SEVENTEENTH_OF_APRIL_2024 = '2024-04-17'
+TEN_AM = '10:00'
+
+VALID_STUDENT_PASSWORD = 'password'
+VALID_STUDENT_EMAIL = 'student@example.com'
+VALID_STUDENT_USERNAME = 'student'
+
+VALID_ADVISOR_EMAIL = 'advisor@example.com'
+VALID_ADVISOR_PASSWORD = 'password'
+VALID_ADVISOR_USERNAME = 'advisor'
+
 
 class MeetingManagementTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
-        self.advisor = Advisor.objects.create_user(username='advisor', email='advisor@example.com', password='password')
-        self.student = Student.objects.create_user(username='student', email='student@example.com', password='password')
+        self.advisor = Advisor.objects.create_user(username=VALID_ADVISOR_USERNAME, email=VALID_ADVISOR_EMAIL,
+                                                   password=VALID_ADVISOR_PASSWORD)
+        self.student = Student.objects.create_user(username=VALID_STUDENT_USERNAME, email=VALID_STUDENT_EMAIL,
+                                                   password=VALID_STUDENT_PASSWORD)
 
         self.student.advisor = self.advisor
         self.student.save()
 
     def test_create_meeting_success(self):
-        self.client.login(username='advisor', password='password')
+        self.client.login(username=VALID_STUDENT_USERNAME, password=VALID_STUDENT_PASSWORD)
 
         meeting_data = {
             'id': self.student.id,
-            'date': '2024-04-17',
-            'time': '10:00'
+            'date': SEVENTEENTH_OF_APRIL_2024,
+            'time': TEN_AM
         }
 
         response = self.client.post(reverse('advisor-meeting-for-student'), data=json.dumps(meeting_data),
                                     content_type='application/json')
-
-        print(response.content)
 
         self.assertEqual(response.status_code, 201)
         self.assertTrue('meeting_id' in response.json())
@@ -36,11 +47,11 @@ class MeetingManagementTestCase(TestCase):
         self.assertIn(response.json()['meeting_id'], student.advisor_meeting_ids)
 
     def test_create_meeting_missing_fields(self):
-        self.client.login(username='advisor', password='password')
+        self.client.login(username=VALID_STUDENT_USERNAME, password=VALID_STUDENT_PASSWORD)
 
         meeting_data = {
             'id': self.student.id,
-            'time': '10:00'
+            'time': TEN_AM
         }
 
         response = self.client.post(reverse('advisor-meeting-for-student'), data=json.dumps(meeting_data),
@@ -50,12 +61,12 @@ class MeetingManagementTestCase(TestCase):
         self.assertTrue('error' in response.json())
 
     def test_create_meeting_student_not_found(self):
-        self.client.login(username='advisor', password='password')
+        self.client.login(username=VALID_STUDENT_USERNAME, password=VALID_STUDENT_PASSWORD)
 
         meeting_data = {
             'id': 9999,
-            'date': '2024-04-17',
-            'time': '10:00'
+            'date': SEVENTEENTH_OF_APRIL_2024,
+            'time': TEN_AM
         }
 
         response = self.client.post(reverse('advisor-meeting-for-student'), data=json.dumps(meeting_data),
@@ -65,15 +76,15 @@ class MeetingManagementTestCase(TestCase):
         self.assertTrue('error' in response.json())
 
     def test_create_meeting_no_advisor_assigned(self):
-        self.client.login(username='advisor', password='password')
+        self.client.login(username=VALID_STUDENT_USERNAME, password=VALID_STUDENT_PASSWORD)
 
         self.student.advisor = None
         self.student.save()
 
         meeting_data = {
             'id': self.student.id,
-            'date': '2024-04-17',
-            'time': '10:00'
+            'date': SEVENTEENTH_OF_APRIL_2024,
+            'time': TEN_AM
         }
 
         response = self.client.post(reverse('advisor-meeting-for-student'), data=json.dumps(meeting_data),
