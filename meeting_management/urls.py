163bee61a99f87d840fd195046f684789c88eab8
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-18 17:26:04 +0100

    FYP-31 handling incoming get request for student meetings

--- meeting_management/urls.py
+++ meeting_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from meeting_management.views import create_meeting_student
+from meeting_management.views import create_meeting_student, get_student_meetings
 
 urlpatterns = [
     path('advisor-meeting-for-student/', create_meeting_student, name='advisor-meeting-for-student'),
+    path('student-meetings', get_student_meetings, name='student-meetings'),
 ]
--- meeting_management/views.py
+++ meeting_management/views.py
@@ -42,3 +42,26 @@ def create_meeting_student(request):
         return JsonResponse({'success': True, 'meeting_id': meeting.id}, status=201)
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+@login_required
+def get_student_meetings(request):
+    if request.method == 'GET':
+        student_id = request.GET.get('id')
+        try:
+            student = Student.objects.get(id=student_id)
+        except Student.DoesNotExist:
+            return JsonResponse({'error': 'Student not found'}, status=404)
+
+        if not student_id:
+            return JsonResponse({'error': 'Student ID is required'}, status=400)
+
+        try:
+            student_meetings = Meeting.objects.filter(student_id=student.id)
+            meetings_data = [{'id': meeting.id, 'date': meeting.date, 'time': meeting.time} for meeting in
+                             student_meetings]
+            return JsonResponse({'meetings': meetings_data}, status=200)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
