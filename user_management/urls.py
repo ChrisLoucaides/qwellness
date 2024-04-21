7d0aeac1dfc988727309930987ef8f405aa8bca9
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-21 13:22:33 +0100

    redirecting empty path to login

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
 from user_management.views import signup, user_login, dashboard, get_user_info, user_logout, filter_advisors_students
 
 urlpatterns = [
     # Other URL patterns
+    path('', RedirectView.as_view(url='/login', permanent=False)),
     path('signup/', signup, name='signup'),
     path('login/', user_login, name='login'),
     path('user-info/', get_user_info, name='user_info'),
